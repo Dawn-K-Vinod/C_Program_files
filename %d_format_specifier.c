a4e8e471fe0eaa7e4f3a081a3093606f0728ab41
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2025-01-24 22:08:14 +0530

    Create %d_format_specifier.c
    Using the ` %d ` format specifier to display an integer value

---
+++ %d_format_specifier.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//using format specifiers inside the printf() statement.
+int main() {
+    int value;
+    value = 12;
+    printf("Displaying an integer: %d",value);
+    return 0;
+}
