5d410fc116a67c677792b0af5b68af236818660c
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-01-28 23:04:28 +0530

    Create swap_two_variables.c

---
+++ swap_two_variables.c
@@ -0,0 +1,12 @@
+// program to swap two variables without a third variable
+#include <stdio.h>
+
+int main() {
+    int a=5,b=3;
+    printf("Before swaping, \na:%d \nb:%d\n\n",a,b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("After swaping, \nA:%d \nB:%d\n",a,b);
+    return 0;
+}
