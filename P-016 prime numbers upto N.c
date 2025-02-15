0ec192eda4848a75c98317dd5b30cf6f80c55c70
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-02-16 00:13:04 +0530

    Update P-016 prime numbers upto N.c

--- P-016 prime numbers upto N.c
+++ P-016 prime numbers upto N.c
@@ -3,13 +3,20 @@ Author     : Dawn K Vinod
 Date       : 04/02/2025
 Description: Program to print Prime numbers upto a number.
 */
+
 #include <stdio.h>
 
 int main() {
     int n;
-    printf("enter :");
+    printf("Enter a number: ");
     scanf("%d",&n);
     
+    if (n>=2) {
+        printf("2 ");
+    } else {
+        printf("Invalid!! Prime numbers starts from 2.");
+        return 0;
+    }
     for (int num=1;num<=n;num+=2) {
         int i=2;
         for (;i<=num;i++) {
