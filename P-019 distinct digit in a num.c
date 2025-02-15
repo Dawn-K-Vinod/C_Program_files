b4d802fd0e60f7fc53116efe503e06b22b9078e8
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-02-15 23:55:47 +0530

    Update and rename P-019 distinct digit in a num, in words.c to P-019 distinct digit in a num.c

--- P-019 distinct digit in a num.c
+++ P-019 distinct digit in a num.c
@@ -1,9 +1,16 @@
+/*
+Author      : Dawn K Vinod
+Date        : 15/02/2025
+Description : Program to display the distinct digits from the given number.
+*/
+
 #include <stdio.h>
 int main () {
 	int num;
 	printf("Enter a number:");
 	scanf("%d",&num);
 	
+	printf("Distinct digits in the number `%d` are: ",num);
 	int num_copy1=num, rem1;
 	//Outer loop for taking each digit from the given number
 	while (num_copy1 != 0) {
@@ -20,19 +27,7 @@ int main () {
 			num_copy2 /= 10;
 		}
 		if (num_copy2==0) {
-			switch (rem1) {
-				case 0: printf("Zero,");break;
-				case 1: printf("One,");break;
-				case 2: printf("Two,");break;
-				case 3: printf("Three,");break;
-				case 4: printf("Four,");break;
-				case 5: printf("Five,");break;
-				case 6: printf("Six,");break;
-				case 7: printf("Seven,");break;
-				case 8: printf("Eight,");break;
-				case 9: printf("Nine,");break;
-				default: printf("Invalid");
-			}
+			printf("%d,",rem1);
 		}
 	}
 	printf("\n");
