4411cab67576584d641983e4153b0642af75c33d
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-03-03 18:38:25 +0530

    Create P-055 str copy.c

---
+++ P-055 str copy.c
@@ -0,0 +1,23 @@
+/*
+Author      : Dawn K Vinod
+Date        : 03/03/2025
+Description : Write a C program to copy one string to another without using strcpy().
+*/
+#include <stdio.h>
+
+int main() {
+    char str[30];
+    printf("Enter the string: ");
+    scanf("%29[^\n]",str);
+    
+    char str_copy[sizeof(str)];
+    
+    int i;
+    for (i=0; str[i]!='\0'; i++) {
+        str_copy[i]=str[i];
+    }
+    str_copy[i]='\0';
+    
+    printf("\nCopy of the string: %s\n",str_copy);
+    return 0;
+}
