27577a45f785611352f28664085b559450203d82
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-03-03 18:12:05 +0530

    Create str length without using strlen().c

---
+++ str length without using strlen().c
@@ -0,0 +1,24 @@
+/*
+Author      : Dawn K Vinod
+Date        : 03/03/2025
+Description : Write a C program to find the length of a string without using strlen().
+*/
+
+#include <stdio.h>
+
+int str_len(char str[]) {
+    int i;
+    for (i=0; str[i]!='\0'; i++);
+    return i;
+}
+
+int main() {
+    char string[30];
+    printf("Enter the string: ");
+    scanf("%29[^\n]",string);
+    
+    int len = str_len(string);
+    
+    printf("\nLength of the given string: %d\n",len);
+    return 0;
+}
