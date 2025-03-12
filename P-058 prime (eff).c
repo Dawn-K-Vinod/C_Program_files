3b35fe0e0660f79dac4fbc22c229f6313f1476d6
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-03-12 15:44:47 +0530

    Create P-058 prime (eff).c

---
+++ P-058 prime (eff).c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+int isprime(int num) {
+    if (num%10==1 || num%10==3 || num%10==7 || num%10==9) {
+        if (num<2) { return 0;}
+        if (num==2) { return 1;}
+        if (num%2==0) { return 0;}
+        
+        for (int i=3; i<sqrt(num)+1; i+=2) {
+            if (num%i==0) { return 0;}
+        }
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    int limit;
+    printf("Enter the limit: ");
+    scanf("%d",&limit);
+    
+    printf("2 3 5 ");
+    int i=1, var1, var2;
+    while (i<limit) {
+        var1=6*i-1;
+        var2=6*i+1;
+        
+        if (var1>limit || var2>limit) { break;}
+        
+        if (isprime(var1)) {printf("%d ",var1);}
+        if (isprime(var2)) {printf("%d ",var2);}
+        i++;
+    }
+    return 0;
+}
