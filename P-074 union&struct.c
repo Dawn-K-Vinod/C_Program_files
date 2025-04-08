c627ce439a26093a23ab30aff2bf48a9b4ff14d9
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-04-08 23:39:42 +0530

    Update P-074 union&struct.c

--- P-074 union&struct.c
+++ P-074 union&struct.c
@@ -15,3 +15,10 @@ int main() {
 	printf("The size of the given union is: %ld\n",sizeof(u));
 	return 0;
 }
+
+/*
+Here note that the size of structure will be displayed as 12 instead of `5(char) + 4(int) = 9` what you expected,
+this is due to a thing called PADDING (https://www.tutorialspoint.com/cprogramming/c_structure_padding_and_packing.htm),
+and this is not a problem, it is actually for the faster accessing of data by CPU, 
+this is based on the CPU architecture.
+*/
