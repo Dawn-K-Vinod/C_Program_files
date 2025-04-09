064472346dd0cc33b435bbe5c8e97c51be9c190e
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-04-09 18:36:22 +0530

    Update P-053 str palindrome or not.c

--- P-053 str palindrome or not.c
+++ P-053 str palindrome or not.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+
+int is_palindrome (char str[]) {
+    int len=strlen(str);
+        int j=len-1;
+
+        int i;
+        char temp;
+        for (i=0; i<len/2; i++,j--) {
+                if (str[i]!=str[j]) {
+                        return 0;
+                }
+        }
+        return 1;
+
+}
+
 int main() {
-	char str[30];
-	printf("Enter the string: ");
-	scanf("%[^\n]",str);
-	
-	int len=strlen(str);
-	int j=len-1;
-	
-	int i;
-	char temp;
-	for (i=0; i<len/2; i++,j--) {
-		if (str[i]!=str[j]) {
-			printf("The given string '%s' is not a palindrome.\n",str);
-			return 0;
-		}
-	}
-	printf("The given string '%s' is a palindrome.\n",str);
-	return 0;
+        char str[30];
+        printf("Enter the string: ");
+        scanf("%[^\n]",str);
+
+        if (is_palindrome(str)){
+        printf("The given string '%s' is a palindrome.\n",str);
+        }
+        else {
+            printf("The given string '%s' is a palindrome.\n",str);
+        }
+        return 0;
 }
