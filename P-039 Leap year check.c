6fb4b97035a750eaba1c816c4279ffff1cd83710
Dawn K Vinod <dawnkvinod6@gmail.com>
2025-08-24 07:56:39 +0530

    Update P-039 Leap year check.c

--- P-039 Leap year check.c
+++ P-039 Leap year check.c
@@ -7,10 +7,10 @@ Description : Program to check whether the given year is leap year or not.
 
 int is_leap_year(int y) {
     // variable y means year.
-    if (y%400==0) {return 1;}
-    else if (y%100==0) {return 0;}
-    else if (y%4==0) {return 1;}
-    else {return 0;}
+    if (y%400==0) return 1;
+    else if (y%100==0) return 0;
+    else if (y%4==0) return 1;
+    else return 0;
 }
 
 int main() {
@@ -18,10 +18,7 @@ int main() {
     printf("Enter the year: ");
     scanf("%d",&year);
     
-    if (is_leap_year(year)) {
-        printf("\n%d is a leap year.\n",year);
-    } else {
-        printf("\n%d is not a leap year.\n",year);
-    }
+    if (is_leap_year(year)) printf("\n%d is a leap year.\n",year);
+    else printf("\n%d is not a leap year.\n",year);
     return 0;
 }
